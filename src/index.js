807e5df7eb35a757d8b015ff06d3f47ff2afb547
sputja <sputja@gmail.com>
2020-12-05 19:32:05 +0200

    menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+import './js/theme.js';
+import menuData from './js/menu.json';
+import template from './template/menu.hbs';
+
+const menuRef = document.querySelector('.js-menu');
+const markup = template(menuData);
+console.log(markup);
+menuRef.insertAdjacentHTML('afterbegin', markup);
---
+++ src/js/theme.js
@@ -0,0 +1,37 @@
+// import menu from './js/menu.json';
+// // const menuJson = JSON.parse(menu);
+// module.export = menu;
+
+const refs = {
+  selectTheme: document.querySelector('#theme-switch-toggle'),
+  body: document.querySelector('body'),
+};
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const STORAGE_KEY = 'theme';
+
+const changeTheme = theme => {
+  if (theme === Theme.DARK) {
+    refs.body.classList.toggle(Theme.DARK);
+    refs.body.classList.add(Theme.DARK);
+    refs.selectTheme.checked = true;
+  } else {
+    refs.body.classList.add(Theme.LIGHT);
+    refs.body.classList.remove(Theme.DARK);
+    localStorage.setItem(STORAGE_KEY, Theme.LIGHT);
+  }
+};
+
+changeTheme(localStorage.getItem(STORAGE_KEY));
+
+function onchangeThemeClick() {
+  const newTheme = refs.selectTheme.checked ? Theme.DARK : Theme.LIGHT;
+  localStorage.setItem(STORAGE_KEY, newTheme);
+  changeTheme(newTheme);
+}
+
+refs.selectTheme.addEventListener('change', onchangeThemeClick);
